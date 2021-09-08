677bdb18a51dff74375fa43e65acc5b3fb881377
RafaelRufino <rafaelrufino.52@gmail.com>
2021-09-08 04:24:59 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import DataTable from "components/DataTable";
-import Footer from "components/Footer";
-import Navbar from "components/Navbar";
+import DataTable from "components/DataTable/";
+import Footer from "components/Footer/";
+import Navbar from "components/Navbar/";
 
 function App() {
   return (
     <>
-      <h1 className="text-primary">DevSuperior</h1>
       <Navbar />
       <DataTable />
       <Footer />
