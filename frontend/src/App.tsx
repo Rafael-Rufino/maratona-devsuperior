98b54ecfaec5697da772c2d43d1b3651a58b8cbb
RafaelRufino <rafaelrufino.52@gmail.com>
2021-09-08 03:59:07 -0300

    Adiconando o  Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import Navbar from "components/Navbar";
+
 function App() {
-  return <h1>Hello word</h1>;
+  return (
+    <>
+      <h1 className="text-primary">DevSuperior</h1>
+      <Navbar />
+      <DataTable />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/utils/BarChart.js
@@ -0,0 +1,19 @@
+const options = {
+  plotOptions: {
+      bar: {
+          horizontal: true,
+      }
+  },
+};
+
+const mockData = {
+  labels: {
+      categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+  },
+  series: [
+      {
+          name: "% Sucesso",
+          data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
+      }
+  ]
+};
